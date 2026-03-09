e4f9a6a2460f77a8c3ec73d8c6fdb2e8fa421aab
paskool87 <pascalfebury@yahoo.fr>
2026-03-09 11:19:00 +0100

    structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Home from "./pages/Home"
 import './App.css'
 
 function App() {
-  return (
-    <div>
-      <h1>Kasa</h1>
-    </div>
-  )
-}
+  return <Home />}
 
 export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Header from "../components/Header/Header"
+import Banner from "../components/Banner/Banner"
+import Gallery from "../components/Gallery/Gallery"
+import Footer from "../components/Footer/Footer"
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Gallery />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
