8cc85cf2a8d57d6574aa6ec2420f3555bd67227c
pascal <pascalfebury@yahoo.fr>
2026-03-11 12:03:46 +0100

    css avant scss

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,15 +1,17 @@
-import Card from "../Card/Card"
-import datas from "../../datas/logementsKasa.json"
-import "./Gallery.css"
+import Card from "../Card/Card";
+import datas from "../../datas/logementsKasa.json";
+import "./Gallery.css";
 
 function Gallery() {
   return (
     <div className="gallery">
-      {datas.map((appart) => (
-        <Card key={appart.id} title={appart.title} cover={appart.cover} />
-      ))}
+      <div className="gallery-container">
+        {datas.map((appart) => (
+          <Card key={appart.id} title={appart.title} cover={appart.cover} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Gallery
+export default Gallery;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,20 @@
-import Header from "../components/Header/Header"
-import Banner from "../components/Banner/Banner"
-import Gallery from "../components/Gallery/Gallery"
-import Footer from "../components/Footer/Footer"
+import Header from "../components/Header/Header";
+import Banner from "../components/Banner/Banner";
+import Gallery from "../components/Gallery/Gallery";
+import Footer from "../components/Footer/Footer";
+import "./Home.css";
 
 function Home() {
   return (
     <>
-      <Header />
-      <Banner />
-      <Gallery />
+      <div className="home">
+        <Header />
+        <Banner />
+        <Gallery />
+      </div>
       <Footer />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
