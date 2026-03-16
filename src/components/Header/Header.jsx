c3ceaf3ec364e5770e85e130823f100842c44c79
paskool87 <pascalfebury@yahoo.fr>
2026-03-16 15:24:55 +0100

    home responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,9 @@ function Header() {
       <img src={logo} alt="Kasa logo" />
       <nav>
         <a href="/">Accueil</a>
-        <a href="/about">À propos</a>
+        <a href="/about" className="nav-link">
+          À propos
+        </a>
       </nav>
     </header>
   );
